8f3e330600cf549e99e7d3d2cd1e51379cea3d51
pranjul1996 <pranjul.dhiman@softradix.in>
2024-05-17 17:51:06 +0530

    song time observer is added

--- composeApp/src/iosMain/kotlin/player/AudioPlayer.kt
+++ composeApp/src/iosMain/kotlin/player/AudioPlayer.kt
@@ -4,24 +4,30 @@ import kotlinx.cinterop.CValue
 import kotlinx.cinterop.ExperimentalForeignApi
 import platform.AVFoundation.AVPlayer
 import platform.AVFoundation.AVPlayerItem
+import platform.AVFoundation.AVPlayerItemDidPlayToEndTimeNotification
 import platform.AVFoundation.AVPlayerTimeControlStatusPlaying
+import platform.AVFoundation.addPeriodicTimeObserverForInterval
 import platform.AVFoundation.currentItem
 import platform.AVFoundation.duration
 import platform.AVFoundation.isPlaybackLikelyToKeepUp
 import platform.AVFoundation.pause
 import platform.AVFoundation.play
 import platform.AVFoundation.removeTimeObserver
+import platform.AVFoundation.replaceCurrentItemWithPlayerItem
 import platform.AVFoundation.seekToTime
 import platform.AVFoundation.timeControlStatus
 import platform.CoreMedia.CMTime
 import platform.CoreMedia.CMTimeGetSeconds
 import platform.CoreMedia.CMTimeMakeWithSeconds
+import platform.Foundation.NSNotificationCenter
+import platform.Foundation.NSOperationQueue
 import platform.darwin.Float64
 import platform.darwin.NSEC_PER_SEC
+import platform.posix.index
 import kotlin.time.DurationUnit
 import kotlin.time.toDuration
 
-actual class AudioPlayer actual constructor(playerState: PlayerState) {
+actual class AudioPlayer actual constructor(private val playerState: PlayerState) {
     private val playerItems = mutableListOf<AVPlayerItem>()
     private val avPlayer = AVPlayer()
 
@@ -35,22 +41,13 @@ actual class AudioPlayer actual constructor(playerState: PlayerState) {
 
     @OptIn(ExperimentalForeignApi::class)
     private val observer: (CValue<CMTime>) -> Unit = { time: CValue<CMTime> ->
-        playerState.isBuffering = avPlayer.currentItem?.isPlaybackLikelyToKeepUp() != true
-        playerState.isPlaying = avPlayer.timeControlStatus == AVPlayerTimeControlStatusPlaying
-        val rawTime: Float64 = CMTimeGetSeconds(time)
-        val parsedTime = rawTime.toDuration(DurationUnit.SECONDS).inWholeSeconds
-        playerState.currentTime = parsedTime
-        if (avPlayer.currentItem != null) {
-            val cmTime = CMTimeGetSeconds(avPlayer.currentItem!!.duration)
-            playerState.duration =
-                if (cmTime.isNaN()) 0 else cmTime.toDuration(DurationUnit.SECONDS).inWholeSeconds
-        }
+
     }
 
     private fun setUpAudioSession() {
-        if (currentItemIndex == -1){
+        if (currentItemIndex == -1) {
             onPlay(0)
-        }else{
+        } else {
             avPlayer.play()
             playerState.isPlaying = true
         }
@@ -64,6 +61,43 @@ actual class AudioPlayer actual constructor(playerState: PlayerState) {
     actual fun onPlay(songIndex: Int) {
         playerState.isBuffering = true
         avPlayer.play()
+    }
+@OptIn(ExperimentalForeignApi::class)
+private fun playerTimer(){
+    timeObserver = avPlayer.addPeriodicTimeObserverForInterval(
+        CMTimeMakeWithSeconds(1.0, NSEC_PER_SEC.toInt()),null){time->
+        playerState.isBuffering = avPlayer.currentItem?.isPlaybackLikelyToKeepUp() != true
+        playerState.isPlaying = avPlayer.timeControlStatus == AVPlayerTimeControlStatusPlaying
+        val rawTime: Float64 = CMTimeGetSeconds(time)
+        val parsedTime = rawTime.toDuration(DurationUnit.SECONDS).inWholeSeconds
+        playerState.currentTime = parsedTime
+        if (avPlayer.currentItem != null) {
+            val cmTime = CMTimeGetSeconds(avPlayer.currentItem!!.duration)
+            playerState.duration =
+                if (cmTime.isNaN()) 0 else cmTime.toDuration(DurationUnit.SECONDS).inWholeSeconds
+        }
+    }
+    NSNotificationCenter.defaultCenter.addObserverForName(
+        name = AVPlayerItemDidPlayToEndTimeNotification,
+        `object` = avPlayer.currentItem,
+        queue = NSOperationQueue.mainQueue,
+        usingBlock = {
+            onNext()
+        }
+    )
+}
+    private fun playSong(songIndex: Int) {
+        if (songIndex >= 0 && songIndex < playerItems.size) {
+            stop()
+            playerState.isBuffering = true
+            playerState.currentItemIndex = currentItemIndex
+            currentItemIndex = songIndex
+            avPlayer.pause()
+            avPlayer.replaceCurrentItemWithPlayerItem(playerItems[songIndex])
+            avPlayer.play()
+        }
+
+
     }
 
     actual fun onPause() {
